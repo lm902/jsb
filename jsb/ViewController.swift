66c99285392abbab35c9d077edd7d9d3e1e598bc
lm902 <i@lm902.cn>
2020-02-03 13:47:22 -0800

    Initial Commit

---
+++ jsb/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  jsb
+//
+//  Created by 李开元 on 2020/2/3.
+//  Copyright © 2020 李开元. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ jsbTests/jsbTests.swift
@@ -0,0 +1,34 @@
+//
+//  jsbTests.swift
+//  jsbTests
+//
+//  Created by 李开元 on 2020/2/3.
+//  Copyright © 2020 李开元. All rights reserved.
+//
+
+import XCTest
+@testable import jsb
+
+class jsbTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
